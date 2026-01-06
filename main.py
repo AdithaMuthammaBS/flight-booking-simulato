29b8bc9cc265b7b9663b025ef2bc340ebb6dcce3
Aditha Muthamma B S <muthammaaditha@gmail.com>
2026-01-06 21:51:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -389,3 +389,8 @@ def download_receipt_pdf(pnr: str, db: Session = Depends(get_db)):
         media_type="application/pdf",
         filename=file_path
     )
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
